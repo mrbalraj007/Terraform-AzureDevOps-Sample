6faf721093291452f034ae5a4c45f28e6aaab3e2
Balraj Singh <53593772+mrbalraj007@users.noreply.github.com>
2025-01-09 16:43:18 +1100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "testRG"
-    storage_account_name = "storageacctf834da40"
+    resource_group_name  = "myfirst-demo-09012025-rg"
+    storage_account_name = "storageacct7563c485"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
