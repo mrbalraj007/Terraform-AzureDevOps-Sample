ec0473e3d36f0d66c7cefa0d52b52b93dab6a783
Balraj Singh <xyz@something.com>
2025-01-10 10:59:28 +1100

    updated files

--- provider.tf
+++ provider.tf
@@ -4,10 +4,20 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+}
+
+# Configure the Azure DevOps Provider
+provider "azuredevops" {
+  org_service_url       = "https://dev.azure.com/YOUR_ORG_NAME"
+  personal_access_token = var.azuredevops_pat
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
 variable "prefix" {
-
+  default = "singh"
 }
 
 variable "location" {
-
+  default = "westus2"
 }
+
+# variable "azuredevops_pat" {
+#   description = "The personal access token for Azure DevOps"
+#   type        = string
+# }
